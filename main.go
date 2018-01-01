0deda696a1850a0376bca2c60518d1d0d6727e01
jarensaa <jens.rensaa@gmail.com>
2018-01-01 15:29:39 +0100

    fix bug with template file pointer

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("/templates/*.tmpl.html")
+	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
 	router.GET("/", func(c *gin.Context) {
